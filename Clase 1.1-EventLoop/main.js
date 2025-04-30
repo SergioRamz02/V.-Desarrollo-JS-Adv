cad2525538a3926ce09c6adb188f57547fad9cdc
Sergio Ramírez <sergio_esfm02@hotmail.com>
2025-04-29 20:36:47 -0600

    Event Loop

--- Clase 1.1-EventLoop/main.js
+++ Clase 1.1-EventLoop/main.js
@@ -1,9 +1,11 @@
 console.log("A"); //Primero en ejecutarse
 
+//Esto es un Task Queue
 setTimeout( () =>{  //Cuarto en ejecutarse
     console.log("B");
 },0);
 
+//Esto es un microtask Queue
 Promise.resolve.then(() => console.log("C")); //Tercero en ejecutarse
 
 console.log("D"); //Segundo en ejecutarse
